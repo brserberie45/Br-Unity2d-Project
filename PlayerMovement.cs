35038735175b869c20e4ae4a4d891bc931e58bab
brserberie45 <99983735+brserberie45@users.noreply.github.com>
2022-02-22 12:41:03 -0500

    Add files via upload
    New Updated Movement Files though the Jumping is not working.

---
+++ PlayerMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D rb2D;
+    private float moveSpeed;
+    private float jumpForce;
+    private bool isJumping;
+    private float moveHorizontal;
+    private float moveVertical;
+    //Set Base Values for PlayerMovement
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+        moveSpeed = 1.2f;
+        jumpForce = 60f;
+        isJumping = false;
+    }
+    
+
+    private void Update()
+    {
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+    }
+    void FixedUpdate()
+    {
+        if (moveHorizontal > 0.1f || moveHorizontal < -0.1f)
+        {
+            rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
+        }
+        if (isJumping && moveVertical > 0.1f)
+        {
+            rb2D.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            isJumping = false;
+        }
+
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            isJumping = true;
+        }
+            
+    }
+
+}
