51c096a7f7245a14207e5798857e8debe3fc75e9
brserberie45 <99983735+brserberie45@users.noreply.github.com>
2022-02-24 12:53:56 -0500

    Add files via upload

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private float speed;
-    private Rigidbody2D body;
-
-    private void Awake()
+    private Rigidbody2D rb2D;
+    private float moveSpeed;
+    private float jumpForce;
+    private bool isJumping;
+    private float moveHorizontal;
+    private float moveVertical;
+    //Set Base Values for PlayerMovement
+    void Start()
     {
-        body = GetComponent<Rigidbody2D>();    
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+        moveSpeed = 1.2f;
+        jumpForce = 5f;
     }
     
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            speed = 5;
-            body.velocity = new Vector2(body.velocity.x, speed);
-        }
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Jump");
     }
+    void FixedUpdate()
+    {
+        if (moveHorizontal > 0.1f || moveHorizontal < -0.1f)
+        {
+            rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
+        }
+        if (moveVertical < 0.1f || moveVertical > 0.1f)
+        {
+            rb2D.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
+        }
+        
+    }
+
+
 }
